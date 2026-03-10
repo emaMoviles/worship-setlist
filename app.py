887a5e639ac75bd5b53b226ad31e8b787d65678d
emaMoviles <157554974+emaMoviles@users.noreply.github.com>
2026-03-10 01:50:32 -0600

    fix render port

--- app.py
+++ app.py
@@ -92,9 +92,9 @@ def crear_setlist():
 
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True
+        port=port
     )
-
